f7d0d76f71509d71339baf182f51ad26df7debcd
Mehrshad Farzaneh <50589065+mehrshadFarzaneh@users.noreply.github.com>
2023-07-27 10:09:11 +0330

    [Remove] unnecessary thing for production

--- src/app/pipes/date-jalali-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DateJalaliPipePipe } from './date-jalali-pipe.pipe';
-
-describe('DateJalaliPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new DateJalaliPipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
